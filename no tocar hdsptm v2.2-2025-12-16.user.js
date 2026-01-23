67df02394475603124e402cc2b693b958109eccf
0xjc4st <42254060+whjalx@users.noreply.github.com>
2026-01-22 21:02:08 -0600

    Actualizar no tocar hdsptm v2.2-2025-12-16.user.js

--- no tocar hdsptm v2.2-2025-12-16.user.js
+++ no tocar hdsptm v2.2-2025-12-16.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         no tocar hdsptm v2.3
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-22
+// @version      2026-01-23
 // @description  fetch mínimo, DOM mínimo, filtros funcionando
 // @author       0xJc4st
 // @match        https://omsmercurio.soriana.com/*
